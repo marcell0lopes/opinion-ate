5a78f82fa6756e78eb4f7911c52971274e78dd82
Marcello Lopes <marcell0lopes@outlook.com>
2022-03-09 14:18:59 -0300

    Defined both behaviors of RestaurantList

--- tests/unit/components/RestaurantList.spec.js
+++ tests/unit/components/RestaurantList.spec.js
@@ -19,4 +19,41 @@ describe("RestaurantList", () => {
 
     expect(restaurantsModule.actions.load).toHaveBeenCalled();
   });
+
+  it("displays the restaurants", () => {
+    const records = [
+      { id: 1, name: "Sushi Place" },
+      { id: 2, name: "Pizza Place" },
+    ];
+
+    const restaurantsModule = {
+      namespaced: true,
+      state: { records },
+      actions: {
+        load: jest.fn().mockName("load"),
+      },
+    };
+
+    const store = new createStore({
+      modules: {
+        restaurants: restaurantsModule,
+      },
+    });
+
+    const wrapper = mount(RestaurantList, { global: { plugins: [store] } });
+
+    const firstRestaurantName = wrapper
+      .findAll('[data-testid="restaurant"]')
+      .at(0)
+      .text();
+
+    expect(firstRestaurantName).toBe("Sushi Place");
+
+    const secondRestaurantName = wrapper
+      .findAll('[data-testid="restaurant"]')
+      .at(1)
+      .text();
+
+    expect(secondRestaurantName).toBe("Pizza Place");
+  });
 });
